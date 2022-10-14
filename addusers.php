d180dbdcf8fc3a187e560578d621efdd661294b6
Jonesw2006 <114936184+Jonesw2006@users.noreply.github.com>
2022-10-14 16:29:22 +0100

    Updates

---
+++ addusers.php
@@ -0,0 +1,24 @@
+
+<?php
+include_once("connection.php"); 
+switch($_POST["role"]){
+	case "Pupil":
+		$role=0;
+		break;
+	case "Teacher":
+		$role=1;
+		break;
+	case "Admin":
+		$role=2;
+		break;
+}
+
+print_r($_POST);
+echo $_POST["gender"]."<br>";
+echo $_POST["forename"]."<br>";
+echo $_POST["surname"]."<br>";
+echo $_POST["house"]."<br>";
+echo $_POST["year"]."<br>";
+echo $_POST["passwd"]."<br>";
+echo $_POST["role"]."<br>";
+?>
