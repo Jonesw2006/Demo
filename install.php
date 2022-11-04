70b96c1abb63737fff81c40d625c041ec102cc34
Jonesw2006 <114936184+Jonesw2006@users.noreply.github.com>
2022-11-04 12:41:22 +0000

    install page

---
+++ install.php
@@ -0,0 +1,45 @@
+
+
+
+<?php
+//tblusers
+include_once("connection.php");
+$stmt = $conn->prepare("DROP TABLE IF EXISTS TblUser;
+CREATE TABLE TblUsers 
+(UserID INT(4) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+Gender VARCHAR(1) NOT NULL,
+Surname VARCHAR(20) NOT NULL,
+Forename VARCHAR(20) NOT NULL,
+Password VARCHAR(20) NOT NULL,
+House VARCHAR(20) NOT NULL,
+Year INT(2) NOT NULL,
+Role TINYINT(1))");
+$stmt->execute();
+$stmt->closeCursor();
+?>
+
+<?php
+//tblsubjects
+include_once("connection.php");
+$stmt = $conn->prepare("DROP TABLE IF EXISTS TblSubjects;
+CREATE TABLE TblSubjects(SubjectID INT(4) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+Subjectname VARCHAR(20) NOT NULL,
+Teacher VARCHAR(20) NOT NULL)");
+$stmt->execute();
+$stmt->closeCursor();
+?>
+
+<?php
+//tblpupilstudies
+include_once("connection.php");
+$stmt = $conn->prepare("DROP TABLE IF EXISTS TblPupilStudies;
+CREATE TABLE TblPupilStudies(Subjectid INT(4),
+Userid INT(4),
+Classposition INT(2),
+Classgrade  CHAR(1),
+Exammark INT(2),
+Comment TEXT,
+PRIMARY KEY(Subjectid,Userid))");
+$stmt->execute();
+$stmt->closeCursor();
+?>
